e86e8354068c7803fea7494af412e36a991ad8af
NoamNeo <gabriel.loira.lorenzo@gmail.com>
2025-10-14 13:29:26 +0200

    Realizamos el 1er ejercicio

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
-
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+import java.util.Scanner;
+public class Main{
+    public void main(String[] args){
+       Scanner usrInput = new Scanner(System.in);
+        //Ejercicio básico bloques Minecraft
+        int bloques;
+        System.out.println("Escibre cuántos bloques quieres picar: ");
+        bloques = usrInput.nextInt();
+        for(int i = 0; i < bloques; i++){
+            System.out.println("Picando bloque " + (i+1) + "...");
+        }
     }
 }
