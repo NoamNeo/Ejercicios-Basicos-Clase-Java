3f5f30c6bf548f70385b5d3d12fcb73ded724d4d
NoamNeo <gabriel.loira.lorenzo@gmail.com>
2025-10-14 13:46:32 +0200

    Realizamos el 2º ejercicio

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,24 @@ public class Main{
         for(int i = 0; i < bloques; i++){
             System.out.println("Picando bloque " + (i+1) + "...");
         }
+
+        //Ejercicio Saldo y precio del refresco
+        double dinero;
+        int decision;
+        String[] bebidas = {"Fanta", "Copa de vino", "Roncola"};
+        double[] precio = {1.5, 5, 7.5};
+        System.out.println("Pon cuanto dinero tienes (para usar decimales usa .)");
+        dinero =  usrInput.nextDouble();
+        System.out.println("Escribe la bebida que quieres");
+        for(int i = 0; i < bebidas.length; i++){
+            System.out.println(i + ": " +bebidas[i]);
+            System.out.println("Precio: " + precio[i]);
+        }
+        decision = usrInput.nextInt();
+        if (dinero < precio[decision]){
+            System.out.println("Precio invalido");
+        }else if (dinero >= precio[decision]){
+            System.out.println("Has comprado " + bebidas[decision]);
+        }
     }
 }
